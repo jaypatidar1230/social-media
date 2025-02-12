f125cb5ddda0231ae27bec2f9f5429edad99e417
Jay <11a21230jay2019@gmail.com>
2025-02-12 14:37:35 +0530

    store done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
-import Loader from "./components/loader"
-function App() {
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Loader } from "./components";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
-  return (
-    <>
-      <h1>Jay</h1>
-      <Loader />
-    </>
-  )
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((useData) => {
+        if (useData) {
+          dispatch(login({ useData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+  return !loading ? <div className="bg-red-">jay</div> : <Loader />;
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Loader from "./Loader";
+
+export { Header, Footer, Loader };
