5c422eb656b1fafdd256a07f49fdad3709049e09
Jay <11a21230jay2019@gmail.com>
2025-02-18 10:32:35 +0530

    login signup

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Loader from "./Loader";
-import LogoutBtn from "./Header/LogoutBtn"
-import Container from "./container/container"
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/container";
+import Button from "./Button";
+import Input from "./Input";
 
-export { Header, Footer, Loader, LogoutBtn, Container };
+export { Header, Footer, Loader, LogoutBtn, Container, Button,  Input };
